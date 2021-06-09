8d3118793bbbeb26d0ce7b8108bfb74a091f2527
Dave L Clark I <davelclark1@gmail.com>
2021-06-09 14:18:57 -0400

    Add files via upload

---
+++ GETMAILA.C
@@ -0,0 +1,5 @@
+       01  GETMAILA-CONTROLS.
+         03  GETMAILA                  PIC  X(8)    VALUE 'GETMAILA'.
+         03  GETMAILA-COMMAREA.
+           05  MAIL-REQUID             PIC  X(8).
+           05  MAIL-ADDRESS            PIC  X(40).
